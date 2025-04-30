1ca1945dc38d45f3cc420e6fb37421131455fd48
Owen Heidtke <owenheidtke@Ovens-MacBook-Pro-2.local>
2025-04-29 23:18:35 -0500

    commit1

--- app.py
+++ app.py
@@ -42,13 +42,6 @@ def compare_route(date):
 
     return jsonify(results)
 
-@app.route('/about')
-def aboutpage():
-    return "Hello, this is the about page."
-
-@app.route('/<favorite_number>', strict_slashes=False)
-def favorite_number(favorite_number):
-    return f"Your favorite number is {favorite_number}."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
